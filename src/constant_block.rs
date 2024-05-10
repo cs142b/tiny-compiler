a5ff6c7ff7bf435838987a9586fb1cacedac388f
dillonct <dillonctran@gmail.com>
2024-05-09 20:02:36 -0700

    finished constant_block.rs

--- src/constant_block.rs
+++ src/constant_block.rs
@@ -1,30 +1,35 @@
 use std::collections::HashMap;
+use crate::instruction::{Instruction, Operation};
 
 pub struct ConstantBlock {
-    constant_table: HashMap<isize, isize>,
-    line_number: isize,
+    constant_table: HashMap<isize, Instruction>,
 }
 
 impl ConstantBlock {
     /// Creates and returns a new constant block
     pub fn new() -> Self {
         ConstantBlock {
-            constant_table: HashMap::<isize, isize>::new(),
-            line_number: -1,
+            constant_table: HashMap::<isize, Instruction>::new(),
         }
     }
 
     /// Adds a constant into the table
     pub fn add_constant(&mut self, constant: isize) {
-        self.constant_table.insert(constant, self.line_number);
-        self.line_number -=1;
+        let operation = Operation::Const(constant);
+        let line_number = (constant_table.len() + 1) * -1;
+        let instruction = Instruction::create_instruction(line_number, operation);
+
+        self.constant_table.insert(constant, instruction);
     }
 
     /// Gets a constant from the table and returns the line number
     pub fn get_constant(&self, constant: isize) -> isize {
         match self.constant_table.get(isize) {
-            Some(constant) => constant,
-            None => (),
+            Some(instruction) => instruction.line_number,
+            None => {
+                self.add_constant(constant);
+                self.get_constant(constant);
+            }
         }
     }
 }
