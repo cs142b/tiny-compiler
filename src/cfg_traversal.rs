dab9a925b82b1bac7136c72731ebdbf670c23653
Andre Khmelnitsky <akhmelni@uci.edu>
2024-06-12 22:43:17 -0700

    added all helper functions

--- src/cfg_traversal.rs
+++ src/cfg_traversal.rs
@@ -59,12 +59,46 @@ fn traverse_in_order_follow_thru_while(start: NodeIndex, v: &mut Instructions, g
     // it will always restart looking at the join block after its own 
     ni
 }
-fn traverse_in_order_helper_branch_or_follow(start: NodeIndex, v: &mut Instructions, g: &mut BasicBlockGraph) -> NodeIndex{
-    let ni = NodeIndex::<u32>::new(0);
-    //should iterate thru til join block
-    // can never stop at join block that is not its own because of the virtue of the function
-    // it will always restart looking at the join block after its own 
-    ni
+// fn traverse_while_follow(start: NodeIndex, v: &mut Instructions, g: &mut BasicBlockGraph) -> NodeIndex{
+//     let ni = NodeIndex::<u32>::new(0);
+
+//     // this should be given to start the follow block of the while loop 
+//     //should iterate thru til join block
+//     // can never stop at join block that is not its own because of the virtue of the function
+//     // it will always restart looking at the join block after its own 
+//     // this will stop at its own join block and then return that block
+
+//     let mut frontier : VecDeque<NodeIndex> = VecDeque::new(); 
+
+//     let children = g.neighbors_directed(start, Outgoing);
+//     for child in children {
+//         let child_bb
+//     }
+
+//     traverse_til_join_given_frontier(&mut frontier, v, g);
+//     ni
+// }
+
+fn traverse_if_branch (start: NodeIndex, v: &mut Instructions, g: &mut BasicBlockGraph) -> NodeIndex {
+    // returns the join block at which point the next function layer can go straight down again
+     
+    traverse_til_join(start, v, g).unwrap()
+}
+
+fn traverse_if_fall_thru(start: NodeIndex, v: &mut Instructions, g: &mut BasicBlockGraph) {
+    traverse_til_join(start, v, g);
+}
+
+fn traverse_while_follow(parent: NodeIndex, v: &mut Instructions, g: &mut BasicBlockGraph) {
+    // let mut frontier = VecDeque::<NodeIndex<u32>>::new(); 
+    let mut start: Option<NodeIndex> = None;
+    for child in g.neighbors_directed(parent, Outgoing) {
+        if g[child].block_type == BasicBlockType::FallThrough {
+            start = Some(child);
+        }
+    }
+
+    traverse_straight_til_conditional(start.unwrap(), v, g);
 }
 fn traverse_straight_til_conditional (start: NodeIndex, v: &mut Instructions, g: &mut BasicBlockGraph) -> (Option<NodeIndex>, Option<ConditionalType>) {
     let mut frontier: VecDeque<NodeIndex> = VecDeque::new();
@@ -78,14 +112,16 @@ fn traverse_straight_til_conditional (start: NodeIndex, v: &mut Instructions, g:
 
         if curr_bb.block_type == BasicBlockType::Conditional {
             if is_if(curr_node, g) {
-                traverse_in_order_helper_fall_thru_if(curr_node, v, g);
+                traverse_if_fall_thru(curr_node, v, g);
                 let branch_block = select_non_fall_thru(curr_node, g); 
-                frontier.push_front(traverse_in_order_helper_branch_or_follow(branch_block.unwrap(), v, g));
+                frontier.push_front(traverse_if_branch(branch_block.unwrap(), v, g));
 
             } else {
-                traverse_in_order_follow_thru_while(curr_node, v, g);
+
+                // traverse_in_order_follow_thru_while(curr_node, v, g);
+                traverse_while_follow(curr_node, v, g);
                 let follow_block = select_non_fall_thru(curr_node, g);
-                frontier.push_front(traverse_in_order_helper_branch_or_follow(follow_block.unwrap(), v, g));
+                frontier.push_front(follow_block.unwrap());
             }
         } else {
             let children = g.neighbors_directed(curr_node, Outgoing); 
@@ -100,50 +136,10 @@ fn traverse_straight_til_conditional (start: NodeIndex, v: &mut Instructions, g:
     (None, None)
 }
 
-fn traverse_straight_til_block (start: NodeIndex, v: &mut Instructions, g: &mut BasicBlockGraph, bbtype: BasicBlockType) -> Option<NodeIndex> {
-    let mut frontier: VecDeque<NodeIndex> = VecDeque::new();
-    frontier.push_back(start);
-
-    while !frontier.is_empty() {
-        let curr_node = frontier.pop_front().unwrap(); 
-
-        let curr_bb = &mut g[curr_node];
-        v.append(&mut curr_bb.instructions); 
-
-        if curr_bb.block_type == bbtype {
-
-            return Some(curr_node);
-            // if is_if(curr_node, g) {
-            //     traverse_in_order_helper_fall_thru_if(curr_node, v, g);
-            //     let branch_block = select_non_fall_thru(curr_node, g); 
-            //     frontier.push_front(traverse_in_order_helper_branch_or_follow(branch_block.unwrap(), v, g));
-
-            // } else {
-            //     traverse_in_order_follow_thru_while(curr_node, v, g);
-            //     let follow_block = select_non_fall_thru(curr_node, g);
-            //     frontier.push_front(traverse_in_order_helper_branch_or_follow(follow_block.unwrap(), v, g));
-            // }
-        } else {
-            let children = g.neighbors_directed(curr_node, Outgoing); 
-            for child in children {
-                frontier.push_back(child);
-            }
-        }
-
-
-        // match curr_bb.block_type {
-        //     BasicBlockType::Conditional => {
-                
-        //     }
-        // }
-    }
-
-    None
-}
-
 // returns the join block that it traverses to
-fn traverse_til_join_given_frontier(frontier: &mut VecDeque<NodeIndex>, v: &mut Instructions, g: &mut BasicBlockGraph, ) -> Option<NodeIndex>{
-
+fn traverse_til_join(start: NodeIndex, v: &mut Instructions, g: &mut BasicBlockGraph, ) -> Option<NodeIndex>{
+    let mut frontier: VecDeque<NodeIndex> = VecDeque::new(); 
+    frontier.push_back(start);
     while !frontier.is_empty() {
         let curr_node = frontier.pop_front().unwrap(); 
 
@@ -151,7 +147,6 @@ fn traverse_til_join_given_frontier(frontier: &mut VecDeque<NodeIndex>, v: &mut 
         v.append(&mut curr_bb.instructions); 
 
         if curr_bb.block_type == BasicBlockType::Join{
-
             return Some(curr_node)
         } else {
             let children = g.neighbors_directed(curr_node, Outgoing); 
